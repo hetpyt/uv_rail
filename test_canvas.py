e70a3a9451ea4034df227356b32eb9397bca9870
hetpyt <43849401+hetpyt@users.noreply.github.com>
2020-02-26 16:33:50 +0300

    Update test_canvas.py

--- test_canvas.py
+++ test_canvas.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import tkinter as tk
+from tkinter.filedialog import asksaveasfile, askopenfile
 
 class MainCanvas(tk.Canvas):
     def __init__(self, parent, rows, cols, cell_s, delim_t):
@@ -15,7 +16,10 @@ class MainCanvas(tk.Canvas):
         self.cell_interval = cell_s + delim_t
         self.canvas_width = cols * cell_s + (cols + 1) * delim_t
         self.canvas_height = rows * cell_s + (rows + 1) * delim_t
-
+        
+        self.MOD_BUTTON1 = (1 << 8)
+        self._last_set = None
+        
         #self.data = [[0 for r in range(rows)] for c in range(cols)]
         self.reset()
         
@@ -47,32 +51,137 @@ class MainCanvas(tk.Canvas):
         return col, row
         
     def set_cell(self, col, row, val):
+        if col >= self.cols or row >= self.rows:
+            return
         self.data[col][row] = val
         self.draw_cell(col, row, (self.active_color if val else self.inactive_color))
         
     def draw_cell(self, col, row, color):
         x = col * self.cell_interval + self.delim_thikness
         y = row * self.cell_interval + self.delim_thikness
-        self.create_rectangle(x, y, x + self.cell_size, y + self.cell_size, outline = color, fill = color)
+        self.create_rectangle(x, y, x + self.cell_size, y + self.cell_size, outline = self.delim_color, fill = color)
     
     def invert_cell(self, col, row):
-        self.set_cell(col, row, not self.data[col][row])
+        self.set_cell(col, row, (0 if self.data[col][row] else 1))
             
     def update(self):
         for c in range(self.cols):
             for r in range(self.rows):
                 self.set_cell(c, r, self.data[c][r])
-                
+    
+    def store_data(self):
+        bytes = (self.rows // 8) + (1 if self.rows % 8 else 0)
+        result = 'uint8_t data[' + str(self.cols) + '][' + str(bytes) + '] = {\n'
+        f = asksaveasfile(mode = 'w')
+        if f:
+            for c in range(self.cols):
+                result += '{'
+                sbyte = ''
+                for r in range(self.rows):
+                    sbyte += str(self.data[c][r])
+                    if ((r + 1) % 8) == 0 or r == (self.rows - 1):
+                        # last bit in byte
+                        result += '0b' + ('0' * (8 - len(sbyte))) + sbyte + ','
+                        sbyte = ''
+                    
+                result = result.rstrip(',') + '},\n'
+            result = result.rstrip(',\n') + '};'
+            f.write(result)
+            f.close()
+    
+    def load_data(self):
+        f = askopenfile()
+        if f:
+            sdata = f.read()
+            print(len(sdata))
+            f.close()
+            
+            col_begin = False
+            row_begin = False
+            byte_begin = False
+            dims_begin = False
+            level = 0
+            tdata = []
+            ccol = -1
+            crow = -1
+            sbyte = ''
+            sdim = ''
+            dims = []
+            for char in sdata:
+                if char == '[':
+                    dims_begin = True
+                    
+                elif char == ']':
+                    dims_begin = False
+                    dims.append(int(sdim))
+                    if len(dims) != 2:
+                        raise Exception('Only 2 dimension arrays supported'.format(char))
+                    tdata = [[0 for r in range(dims[0])] for c in range(dims[1])]
+                    print(dims)
+                    
+                elif char == '{':
+                    level += 1
+                    if level == 1:
+                        ccol += 1
+                    elif level == 2:
+                        crow += 1
+                    else:
+                        raise Exception('Unexpected char "{}". Only 2 dimension arrays supported'.format(char))
+                        
+                elif char == '}':
+                    level -= 1
+                    if level == 0:
+                        # all done
+                        pass
+                    elif level == 1:
+                        # col done
+                        if byte_begin:
+                            tdata[ccol][crow] = sbyte
+                        crow = -1
+                    else:
+                        raise Exception('Unexpected char "{}".'.format(char))
+                        
+                    byte_begin = False
+                    
+                elif char == ',':
+                    if level == 2:
+                        # end of byte in row - store
+                        if byte_begin:
+                            tdata[ccol][crow] = sbyte
+                            byte_begin = False
+
+                    elif dims_begin:
+                        dims.append(int(sdim))
+                        
+                elif char == 'b':
+                    # new byte begin with next char
+                    byte_begin = True
+                    sbyte = ''
+                        
+                elif char.isdigit():
+                    if byte_begin:
+                        sbyte += char
+                    elif dims_begin:
+                        sdim += char
+                        
+            print(tdata)
+    
     def on_left_button(self, event):
+        #print(event)
         col, row = self.coord_to_cell(event.x, event.y)
         self.invert_cell(col, row)
+        self._last_set = self.data[col][row]
     
     def on_mouse_move(self, event):
         #print(event)
-        pass
-    
+        if event.state & self.MOD_BUTTON1 == self.MOD_BUTTON1:
+            col, row = self.coord_to_cell(event.x, event.y)
+            self.set_cell(col, row, self._last_set)
+            
+
     def on_button_release(self, event):
-        print(event)
+        #print(event)
+        pass
     
     def on_key_pressed(self, event):
         print(event)
@@ -82,7 +191,14 @@ class MainCanvas(tk.Canvas):
             
         elif event.keycode == 85: # u
             self.update()
-    
+            
+        elif event.keycode == 83: # s
+            self.store_data()
+            
+        elif event.keycode == 76: # l
+            self.load_data()
+            
+            
 if __name__ == '__main__':
     root = tk.Tk()
     canvas = MainCanvas(root, 16, 50, 20, 5)
